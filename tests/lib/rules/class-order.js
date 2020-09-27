12f69698e4fed608c03d33e7e856038a652ffec8
Haseeb Majid <haseeb.majid@sky.uk>
2020-09-27 11:41:09 +0100

    feat: Add Twin Macro
    Add support for twin.macro support in this plugin.

--- tests/lib/rules/class-order.js
+++ tests/lib/rules/class-order.js
@@ -18,6 +18,9 @@ const parserOptions = {
 new RuleTester({parserOptions}).run('class-order', rule, {
   valid: [
     {
+      code: '<button tw="flex mt-2 mt-4">Sign In</button>',
+    },
+    {
       code: '<button class="flex mt-2 mt-4">Sign In</button>',
     },
     {
@@ -29,6 +32,16 @@ new RuleTester({parserOptions}).run('class-order', rule, {
     {
       code: '<button class="flex mt-2 mt-4">Sign In</button>',
       parser: parsers.BABEL_ESLINT,
+    },
+    {
+      code: 'tw.div`flex mt-2 mt-4`',
+    },
+    {
+      code: 'tw`flex mt-2 mt-4`',
+    },
+    {
+      code:
+        'const Input = styled.input`\n${tw`flex mt-2 mt-4`}\ncolor: purple;`',
     },
   ],
 
@@ -42,8 +55,33 @@ new RuleTester({parserOptions}).run('class-order', rule, {
       ],
     },
     {
+      code: '<button tw="mt-4 mt-2 flex">Sign In</button>',
+      errors: [
+        {
+          message: 'Invalid class names order',
+        },
+      ],
+    },
+    {
       code: '<button class="mt-4 mt-2 flex">Sign In</button>',
       parser: parsers.BABEL_ESLINT,
+      errors: [
+        {
+          message: 'Invalid class names order',
+        },
+      ],
+    },
+    {
+      code: 'tw.div`mt-4 mt-2 flex`',
+      errors: [
+        {
+          message: 'Invalid class names order',
+        },
+      ],
+    },
+    {
+      code:
+        'const Input = styled.input`\n${tw`mt-4 mt-2 flex`}\ncolor: purple;`',
       errors: [
         {
           message: 'Invalid class names order',
